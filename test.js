9ff307e825dfb005e30b0aa093379ffe7100ef41
Antonio Santiago Dueñas <antonio.duenas@dowjones.com>
2018-05-08 21:03:45 -0500

    test

---
+++ test.js
@@ -0,0 +1,15 @@
+const sinon = require('sinon');
+const assert = require('assert');
+
+const functionTest = (cb) => {
+	const num = 3;
+	cb(3)
+}
+
+const prueba = () => {
+
+}
+
+it("test should call all subscribers when exceptions:", function () {
+});
+
